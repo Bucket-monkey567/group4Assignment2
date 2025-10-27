2782548c3b4449eedaf84f8214a7d694b856b343
jhamrinal63@gmail.com <jhamrinal63@gmail.com>
2025-10-27 12:16:28 -0600

    added get rear, dequeue, enqueue to queue adt

--- src/utilities/QueueADT.java
+++ src/utilities/QueueADT.java
@@ -13,6 +13,25 @@ import exceptions.EmptyQueueException;
  * @param <T> the type of elements stored in the queue
  */
 public interface QueueADT<T> {
+	
+	/**
+	 * Adds an element to the end of the queue
+	 * <p><b>Precondition:</b> Element must not be null.</p>
+     * <p><b>Postcondition:</b> Queue has a a new element at the rear</p>
+	 */
+	public void enqueue(T element);
+	
+	/**
+	 * Retrieves and removes the front element of the queue
+     *
+     * @return The removed element
+     *
+     * @throws EmptyQueueException if the queue is empty
+     *
+     * <p><b>Precondition:</b> The queue must contain at least one element.</p>
+     * <p><b>Postcondition:</b> the front element of the queue is removed.</p>
+	 */
+	public T dequeue() throws EmptyQueueException;
 
     /**
      * Retrieves (but does not remove) the element at the front of the queue.
@@ -25,6 +44,16 @@ public interface QueueADT<T> {
      * <p><b>Postcondition:</b> The queue remains unchanged.</p>
      */
     public T getFront() throws EmptyQueueException;
+    
+    /**
+     * Retrieves (but does not remove) the element at the rear of the queue.
+     * @return the rear element f the queue
+     * @throws EmptyQueueException
+     * 
+     * <p><b>Precondition:</b> The queue must contain at least one element.</p>
+     * <p><b>Postcondition:</b> The queue remains unchanged..</p>
+     */
+    public T getRear() throws EmptyQueueException;
 
     /**
      * Checks whether the queue contains any elements.
